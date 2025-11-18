f892c228c7b68e2b2b0b461e6bca2a99d8e0e1a5
Abdulrahman Elkenany <boudy.elkenany123@gmail.com>
2025-11-18 02:04:25 +0200

    Update script version to 2.3.0
    version typo

--- spicetify.sh
+++ spicetify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly SCRIPT_VERSION="2.2.1"
+readonly SCRIPT_VERSION="2.3.0"
 readonly SPOTIFY_FLATPAK_ID="com.spotify.Client"
 
 COLOR_RESET='\033[0m'; COLOR_RED='\033[0;31m'; COLOR_GREEN='\033[0;32m'; COLOR_YELLOW='\033[0;33m'
